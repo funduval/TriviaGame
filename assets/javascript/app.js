3a341d9de2313b6f572c8b8bbc50623aacad3e60
funduval <fundaduval@gmail.com>
2017-05-11 13:39:09 -0400

    need to add multiple choice, randomize button placement

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,12 @@
     
     var queryURL = "http://jservice.io/api/clues";
     var trivia = []
-     var correctAnswer = []
+    var correctAnswer = []
+    var closeAnswer =["Arbor Day","The Eastern Conference","Prostitute","The Biography of Leonard Nimoy","peacock and crocodile","The Painted Bird"];
+    var lameAnswer = ["November Solstice","The Eastern Division","Skullery Maid","Picard's Rescue","willoughby and koala","Tess"];
+    var dumbAnswer = ["Taco Tuesday","The Peach Bowl","Chorus Girl","The Final Frontier","jackalope and jaberwock","A Clockword Orange"];
+    var win = 0;
+    var lose = 0;
 
     $.ajax({
       url: queryURL,
@@ -36,7 +41,13 @@ window.onload = function() {
   $("#reset").on("click", stopwatch.reset);
   $("#start").on("click", stopwatch.start);
   $("#start").on("click", triviaPlay.newQuestion);
-  $("#start").on("click", triviaPlay.newQuestion);
+  
+  $("#a").on("click", triviaPlay.win);
+  $("#b").on("click", triviaPlay.lose);
+  $("#c").on("click", triviaPlay.lose);
+  $("#d").on("click", triviaPlay.lose);
+ 
+
 
 };
 
@@ -45,13 +56,6 @@ var intervalId;
 var stopwatch = {
 
   time: 30,
-
-  reset: function() {
-
-    stopwatch.time = 30;
-    $("#display").html(":30");
-
-  },
 
   start: function() {
     
@@ -71,71 +75,68 @@ var stopwatch = {
 }
 };
 
-
-
 var triviaPlay = {
-
-
 
   newQuestion: function() {
 
-    var abcd = ["A","B","C","D"]
+   // var abcd = ["a","b","c","d"]
+   // var anyLetter = Math.floor(Math.random() * (abcd.length+1));
 
    var rando = Math.floor(Math.random() * (trivia.length+1));
 
    $("#bigQuestion").html(trivia[rando]);
 
-   var anyLetter = Math.floor(Math.random() * (abcd.length+1));
-   $("#a").html("  " + correctAnswer[rando]);
    
+   $("#a").html("  " + correctAnswer[rando]);
+   $("#b").html("  " + lameAnswer[rando]);
+   $("#c").html("  " + closeAnswer[rando]);
+   $("#d").html("  " + dumbAnswer[rando]);
 
-
+   
    },
 
   answers: function() {
-
-   
-
-
-
+    // put the answers into the buttons but put the buttons in a random spot (childOf) in the Unordered list
 
   },
-
 
   win: function(){
 
-
+  win++;
+  $("#title").html("<img class='img-responsive' id='clara' src='../TriviaGame/assets/images/Clarabarton.jpg' alt='Image'/>");
+  $("#questions").css("visibility", "hidden");
+   setTimeout(function(){
+                       triviaPlay.reset();
+                }, 2000);
 
   },
 
-
 lose: function(){
 
-
+  lose++;
+  $("#title").html("<img class='img-responsive' id='mrsWard' src='../TriviaGame/assets/images/Mrs_Wardroper.jpg' alt='Image'/>");
+  $("#questions").css("visibility", "hidden");
+    setTimeout(function(){
+                      triviaPlay.reset();
+                }, 2000);
 
 },
 
 reset: function() {
-
+  $("#mrsWard").remove()
+  $("#clara").remove()
+  $("#questions").css("visibility", "visible");
+  $("#questions").html(triviaPlay.newQuestion);
+  
+    stopwatch.time = 30;
+    $("#display").html(":30");
+    stopwatch.start()
 
 }
 
 
-
 }; 
-    // var converted = stopwatch.timeConverter(stopwatch.time);
- // console.log(converted);
 
-    
-
-  // },
-  // timeConverter: function(t) {
-
-  //   var minutes = Math.floor(t / 60);
-  //   var seconds = t - (minutes * 60);
-
-  //   if (seconds < 10) {
-  //     seconds = "0" + seconds;
  
 
    
