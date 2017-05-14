15daa937cc7136fe97e9caf7ab9c461e58833a1a
funduval <fundaduval@gmail.com>
2017-05-13 23:58:18 -0400

    changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,12 +3,12 @@
 var queryURL = "http://jservice.io/api/clues";
 var trivia = []
 var correctAnswer = []
-var closeAnswer = ["Arbor Day", "The Eastern Conference", "Prostitute", "The Biography of Leonard Nimoy", "peacock and crocodile", "The Painted Bird", "Iowa", "Woman", "Coopers", "Mission Impossible", "Oh Suzanna"];
-var lameAnswer = ["November Solstice", "The Eastern Division", "Skullery Maid", "Picard's Rescue", "willoughby and koala", "Tess", "New Hampshire", "Lefty", "Keepers", "Tropic Thunder", "Clementine"];
-var dumbAnswer = ["Taco Tuesday", "The Peach Bowl", "Chorus Girl", "The Final Frontier", "jackalope and jaberwock", "A Clockword Orange", "Ohio", "Arab", "Peepers", "The Scientology Center", "I'll Go A' Ramblin"];
+var closeAnswer = ["Arbor Day", "The Eastern Conference", "Prostitute", "The Biography of Leonard Nimoy", "peacock and crocodile", "The Painted Bird", "Iowa", "Woman", "Coopers", "Mission Impossible", "Oh Suzanna","cat-o-nine-tails","stump speech","Michael Scott","judge","The Man With Two Brains","Banshee","an enzyme","The Torys","Agganis Arena","publishers","Franz","Virginia Woolf","Donald Trump"];
+var lameAnswer = ["November Solstice", "The Eastern Division", "Skullery Maid", "Picard's Rescue", "willoughby and koala", "Tess", "New Hampshire", "Lefty", "Keepers", "Tropic Thunder", "Clementine","mace","soap box","Greg Maddux","prosecutor","Dirty Rotten Scoundrels","Wombat","an electron","Tammany Hall","TD Garden","fishermen","Wolfgang","Martha","Herbert Hoover"];
+var dumbAnswer = ["Taco Tuesday", "The Peach Bowl", "Chorus Girl", "The Final Frontier", "jackalope and jaberwock", "A Clockword Orange", "Ohio", "Arab", "Peepers", "The Scientology Center", "I'll Go A' Ramblin","tourret","road trip","Sandy Koufax","public defender","Dead Men Don't Wear Plaid","Kimodo Dragon","a chain reaction","The Green Party","Three Rivers Stadium","architects","Saglieri","Cleopatra", "Woodrow Wilson"];
 var win = 0;
 var lose = 0;
-
+var questionCounter = 0;
 //call the query and push the data into arrays
 
 $.ajax({
@@ -16,12 +16,12 @@ $.ajax({
     method: "GET"
 }).done(function(response) {
 
-    for (i = 0; i < 50; i++) {
+    for (i = 0; i <= 25; i++) {
 
         trivia.push(response[i].question);
     }
 
-    for (k = 0; k < 50; k++) {
+    for (k = 0; k <= 25; k++) {
 
         correctAnswer.push(response[k].answer);
     }
@@ -32,9 +32,9 @@ $.ajax({
 
 
 window.onload = function() {
-    
+
     $(".stop").on("click", stopwatch.stop);
-   
+
     $("#start").on("click", stopwatch.start);
     $("#start").on("click", triviaPlay.newQuestion);
 
@@ -46,7 +46,7 @@ var intervalId;
 
 var stopwatch = {
 
-    time: 30,
+    time: 20,
 
     start: function() {
 
@@ -63,12 +63,48 @@ var stopwatch = {
         stopwatch.time--;
 
         $("#display").html(":" + stopwatch.time);
+
+
+        if (stopwatch.time === 0) {
+
+            lose++;
+
+            $("#title").html("<img class='img-responsive' id='condensed' src='../TriviaGame/assets/images/CondensedMilkKids.jpg' alt='Image'/>");
+            $("#questions").css("visibility", "hidden");
+            $("#start").html("");
+
+            stopwatch.time = 0;
+            stopwatch.stop();
+
+            var anotherTimer;
+
+            anotherTimer = setTimeout(function() {
+                triviaPlay.reset();
+            }, 4000);
+
+        }
+
+            
+
+           
+
+            
+
+        
     }
 };
 
 var triviaPlay = {
 
+
+
+
     newQuestion: function() {
+
+        $("#questions").css("visibility", "visible");
+
+
+
 
         var rando = Math.floor(Math.random() * (11 + 1));
 
@@ -136,49 +172,258 @@ var triviaPlay = {
             $("#a").on("click", triviaPlay.lose);
             $("#b").on("click", triviaPlay.lose);
         }
+
+
+
+
+
+
     },
 
 
     win: function() {
 
         win++;
+
+        questionCounter++;
+
+        console.log(questionCounter);
+
         $("#title").html("<img class='img-responsive' id='clara' src='../TriviaGame/assets/images/Clarabarton.jpg' alt='Image'/>");
         $("#questions").css("visibility", "hidden");
 
-        var timeVar;
-        timeVar = setTimeout(function() {
-            triviaPlay.reset();
-        }, 2000);
-        // clearTimeout(myVar);
-        reset();
+        if (questionCounter >= 25) {
+
+            triviaPlay.ratings();
+            var timeVar;
+
+            timeVar = setTimeout(function() {
+                triviaPlay.endgame();
+            }, 10000);
+
+        } else {
+            var timeVar;
+
+            timeVar = setTimeout(function() {
+                triviaPlay.reset();
+            }, 2000);
+           
+
+        }
+
+
+        var winBox = $("<div>");
+       
+            winBox.addClass("winScore");
+            winBox.append("Correct: " + win);
+            $("#title").append(winBox);
+
+
 
     },
 
     lose: function() {
 
         lose++;
+
+        questionCounter++;
+
+        console.log(questionCounter);
+       
+
         $("#title").html("<img class='img-responsive' id='mrsWard' src='../TriviaGame/assets/images/Mrs_Wardroper.jpg' alt='Image'/>");
         $("#questions").css("visibility", "hidden");
-        var timeVar;
-        timeVar = setTimeout(function() {
-            triviaPlay.reset();
-        }, 2000);
-        // clearTimeout(myVar);
-        reset();
+
+
+
+        if (questionCounter >= 25) {
+
+            triviaPlay.ratings();
+
+            var newVar;
+
+            newVar = setTimeout(function() {
+                triviaPlay.endgame();
+
+            }, 10000);
+
+        }   
+
+
+        else {
+
+             var newVar;
+
+                newVar = setTimeout(function() {
+                triviaPlay.reset();
+
+            }, 2000);
+
+            
+
+        }
+
+
+         var loseBox = $("<div>");
+       
+            loseBox.addClass("winScore");
+            loseBox.append("Incorrect: " + lose);
+            $("#title").append(loseBox);
+
 
     },
 
     reset: function() {
 
-        $("#mrsWard").remove()
-        $("#clara").remove()
+        $("#condensed").remove();
+        $("#mrsWard").remove();
+        $("#clara").remove();
         $("#questions").css("visibility", "visible");
         $("#questions").html(triviaPlay.newQuestion);
 
-        stopwatch.time = 30;
-        $("#display").html(":30");
+        stopwatch.time = 20;
+        $("#display").html(":20");
         stopwatch.start();
-        triviaPlay();
+        triviaPlay.newQuestion();
+
+    },
+
+    ratings: function() {
+
+        if (win <= 6) {
+
+            $("#title").html("<img class='img-responsive' id='athlete' src='../TriviaGame/assets/images/Athlete.jpg' alt='Image'/>");
+            $("#questions").css("visibility", "hidden");
+            $("#start").css("visibility", "hidden");
+            $("#display").css("visibility", "hidden");
+
+            var ratingAlert = $("<div>");
+            ratingAlert.addClass("userAlert");
+            ratingAlert.text("You're more brawn than brains, it seems. Perhaps become an Athlete of some kind.");
+            $("#title").append(ratingAlert);
+
+
+
+
+            
+            $("body").css("background", "none");
+           
+    
+          
+
+            stopwatch.time = 0;
+            stopwatch.stop();
+
+        }
+
+        if (win > 6 && win <= 15) {
+
+            $("#title").html("<img class='img-responsive' id='apprentice' src='../TriviaGame/assets/images/Apprentice.jpg' alt='Image'/>");
+            $("#questions").css("visibility", "hidden");
+            $("#start").css("visibility", "hidden");
+            $("#display").css("visibility", "hidden");
+
+            var ratingAlert = $("<div>");
+            ratingAlert.addClass("userAlert");
+            ratingAlert.text("You need some mentorship. Perhaps become an...");
+            $("#title").append(ratingAlert);
+
+
+            $("body").css("background", "none");
+            stopwatch.time = 0;
+            stopwatch.stop();
+
+        }
+
+        if (win > 15 && win <=20) {
+
+            
+            $("#title").html("<img class='img-responsive' id='highwayman' src='../TriviaGame/assets/images/Highwayman.jpg' alt='Image'/>");
+            $("#questions").css("visibility", "hidden");
+            $("#start").css("visibility", "hidden");
+            $("#display").css("visibility", "hidden");
+
+            var ratingAlert = $("<div>");
+            ratingAlert.addClass("userAlert");
+            ratingAlert.text("You're smart but light on knowledge. You're more cut out for the life of a...");
+            $("#title").append(ratingAlert);
+
+
+            $("body").css("background", "none");
+            stopwatch.time = 0;
+            stopwatch.stop();
+
+        }
+
+        if (win > 20 && win < 25) {
+
+            $("#title").html("<img class='img-responsive' id='apothecary' src='../TriviaGame/assets/images/Apothecary.jpg' alt='Image'/>");
+            $("#questions").css("visibility", "hidden");
+            $("#start").css("visibility", "hidden");
+            $("#display").css("visibility", "hidden");
+
+            var ratingAlert = $("<div>");
+            ratingAlert.addClass("userAlert");
+            ratingAlert.text("You're very wise indeed. You'd make a grand...");
+            $("#title").append(ratingAlert);
+
+
+            $("body").css("background", "none");
+            stopwatch.time = 0;
+            stopwatch.stop();
+        }
+
+        if (win >= 25) {
+
+            $("#title").html("<img class='img-responsive' id='aeronaut' src='../TriviaGame/assets/images/Aeronaut.jpg' alt='Image'/>");
+            $("#questions").css("visibility", "hidden");
+            $("#start").css("visibility", "hidden");
+            $("#display").css("visibility", "hidden");
+
+            var ratingAlert = $("<div>");
+            ratingAlert.addClass("userAlert");
+            ratingAlert.text("You've gotten a perfect score! You've got the brains of an...");
+            $("#title").append(ratingAlert);
+
+
+            $("body").css("background", "none");
+            stopwatch.time = 0;
+            stopwatch.stop();
+        }
+
+
+    },
+
+    endgame: function() {
+
+        questionCounter = 0;
+        win = 0;
+        lose = 0;
+        $("#athlete").remove()
+        $("#apothecary").remove()
+        $("#apprentice").remove()
+        $("#aeronaut").remove()
+        $("#highwayman").remove()
+
+
+
+      
+        $("#start").css("visibility", "visible");
+        $("#display").css("visibility", "visible");
+        $("#questions").html(triviaPlay.newQuestion);
+         $("#questions").css("visibility", "hidden");
+
+
+
+        
+
+        $("body").css("background-image", "url('../TriviaGame/assets/images/victorianBackground.png')");
+        $(".userAlert").remove();
+
+        stopwatch.time = 20;
+        $("#display").html(":20");
+        $("#start").html("Play Again?")
+
 
     }
 
